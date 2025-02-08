9b5a0c563507564bc7123e11f86420de8d725e0d
ramik1990 <ramikshudabaev@gmail.com>
2025-02-09 02:01:11 +0500

    Feat: Добавил вывод общей информации с базы на главную страницу

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Setting;
 use Illuminate\Http\Request;
+
 
 class SettingController extends Controller
 {
-    //
+    public function getSettings() {
+        $settings = Setting::first();
+        return response()->json($settings);
+    }
 }
--- resources/js/Components/Home.vue
+++ resources/js/Components/Home.vue
@@ -1,21 +1,55 @@
 <template>
-    <h1>Main Page</h1>
+    <section class="mainPage" v-if="settings">
+        <h1 align="center">{{ settings.title }}</h1>
+        <h3 align="center">{{ settings.organization }}</h3>
+        <img :src="settings.imgUrl" :alt="settings.title">
+        <p>{{ settings.description }}</p>
+        <span>Автор: {{ settings.autor }}</span>
+    </section>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     name: "Home",
     data() {
         return {
-
+            settings: null
         }
     },
     methods: {
-        
+        getSettings() {
+            axios.get('/api/getSettings').then( response => {
+                console.log(response.data);
+                this.settings = response.data;
+            });
+        }
+    },
+    created() {
+        this.getSettings();
     },
 }
 </script>
 
 <style scoped>
-
+.mainPage {
+    width: 100%;   
+    display: flex;
+    flex-direction: column; 
+    justify-content: center;
+    align-items: center;
+}
+.mainPage h1 {
+    text-transform: uppercase;
+    font-weight: bolder;
+}
+.mainPage p {
+    font-size: 20px;
+    width: 50%;
+}
+.mainPage span {
+    font-weight: bold;
+    font-size: 18px;
+}
 </style>
